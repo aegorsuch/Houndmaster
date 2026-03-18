dcc1666405c004ce660f46d5951b535e8e86c7fb
aegorsuch <alex@weartak.com>
2026-03-18 15:31:03 -0500

    Update BloodhoundOrderManager.java: latest changes

--- app/src/main/java/com/atakmap/android/helloworld/plugin/BloodhoundOrderManager.java
+++ app/src/main/java/com/atakmap/android/helloworld/plugin/BloodhoundOrderManager.java
@@ -4,14 +4,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BloodhoundOrderManager {
+    private static final BloodhoundOrderManager INSTANCE = new BloodhoundOrderManager();
     private final List<BloodhoundOrder> orders = new ArrayList<>();
+
+    // Listener interface and support
+    public interface OrderChangeListener {
+        void onOrdersChanged();
+    }
+    private final List<OrderChangeListener> listeners = new ArrayList<>();
+
+    public void addOrderChangeListener(OrderChangeListener listener) {
+        listeners.add(listener);
+    }
+
+    public void removeOrderChangeListener(OrderChangeListener listener) {
+        listeners.remove(listener);
+    }
+
+    private void notifyOrderChange() {
+        for (OrderChangeListener l : listeners) {
+            l.onOrdersChanged();
+        }
+    }
+
+    private BloodhoundOrderManager() {}
+
+    public static BloodhoundOrderManager getInstance() {
+        return INSTANCE;
+    }
 
     public void addOrder(BloodhoundOrder order) {
         orders.add(order);
+        notifyOrderChange();
     }
 
     public void removeOrder(BloodhoundOrder order) {
         orders.remove(order);
+        notifyOrderChange();
     }
 
     public List<BloodhoundOrder> getOrders() {
@@ -27,4 +56,3 @@ public class BloodhoundOrderManager {
         return null;
     }
 }
-
